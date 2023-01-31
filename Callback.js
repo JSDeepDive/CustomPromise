04fa07c75d20a088ba493e3048bc1545bdadf6c8
ericagong <48196721+ericagong@users.noreply.github.com>
2023-01-31 14:18:10 +0900

    feat: refactoring

--- Callback.js
+++ Callback.js
@@ -1,19 +1,19 @@
-const conditionA = false;
-const conditionB = false;
+const conditionA = false
+const conditionB = false
 
 function myCallback(successCb, failCb) {
   if (!conditionA) {
     failCb({
       name: "Condition A failed",
       message: ":(",
-    });
+    })
   } else if (!conditionB) {
     failCb({
       name: "Condition B failed",
       message: "OMG",
-    });
+    })
   } else {
-    successCb("SUCCESS!");
+    successCb("SUCCESS!")
   }
 }
 
@@ -24,33 +24,33 @@ function myPromise() {
       reject({
         name: "Condition A failed",
         message: ":(",
-      });
+      })
     } else if (!conditionB) {
       reject({
         name: "Condition B failed",
         message: "OMG",
-      });
+      })
     } else {
-      resolve("SUCCESS!");
+      resolve("SUCCESS!")
     }
-  });
+  })
 }
 
 myCallback(
   (message) => {
-    console.log("Success case: ", message);
+    console.log("Success case: ", message)
   },
   (error) => {
-    console.log(error.name, error.message);
+    console.log(error.name, error.message)
   }
-);
+)
 
 // then, catch, finally 메서드 인터페이스 통해 콜백 함수를 등록.
 // 특히 nested callback을 promise chaining을 통해 가독성 좋게 작성 가능.
 myPromise()
   .then((message) => {
-    console.log("Success case: ", message);
+    console.log("Success case: ", message)
   })
   .catch((error) => {
-    console.log(error.name, error.message);
-  });
+    console.log(error.name, error.message)
+  })
--- README.md
+++ README.md
@@ -1,18 +1,1 @@
 # 커스텀 Promise 구현하기
-
-- 목표: Vanilla JS로 Promise를 직접 구현해서 내부 동작 방식에 대해 이해해보자.
-
-# 구현 현황
-
-- 기본
-  [V] Promise vs Callback
-  [V] Promise 클래스 구현
-- prototype methods
-  [V] then 메서드 구현
-  [V] catch 메서드 구현
-  [V] finally 메서드 구현
-- static methods
-  [V] all 메서드 구현
-  [V] allSettled 메서드 구현
-  [V] race 메서드 구현
-  [V] any 메서드 구현
